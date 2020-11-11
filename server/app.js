1197be4af84829bfa03962d5fccfd34db5bc3059
Nicolas Müller <nicolas.mueller@bit.admin.ch>
2020-11-11 12:57:27 +0100

    make post request for initialize

--- server/app.js
+++ server/app.js
@@ -3,21 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var mysqlRouter = require('./routes/mysql');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/mysql', mysqlRouter);
@@ -35,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send(err.message);
 });
 
 module.exports = app;
--- server/routes/mysql.js
+++ server/routes/mysql.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection;
 
-/* GET users listing. */
+/* GET mysql infos */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('mysql infos');
 });
+
+/* Initialize SQL Connection */
+router.post('/initialize', function(req, res, next) {
+  resetConnection();
+  const credentials = req.body;
+  connection = mysql.createConnection(credentials);
+  connection.connect(function(err){
+    if(err){
+      res.status(400).send("Unable to connect to database. Connection string might be incorrect")
+      return;
+    }
+    res.send('MySQL Server successfully conected with connection string: ' + credentials);
+  });
+});
+
+router.get('/end', function(req, res, next) {
+  resetConnection();
+  return res.send("")
+});
+
+function resetConnection() {
+  if(this.connection){
+    this.connection.end();
+  }
+}
+
+function testConnection() {
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+  });
+}
+
+router.get('')
 
 module.exports = router;
